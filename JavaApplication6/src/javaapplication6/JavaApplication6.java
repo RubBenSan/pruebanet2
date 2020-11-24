26f46aa4fcef105c82fb05981ee3f59f4c9c4c9c
Ruben <rub_ben_san@hotmail.com>
2020-11-24 23:14:44 +0100

    version modificada (añadido de =)

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -16,6 +16,7 @@ public class JavaApplication6 {
      */
     public static void main(String[] args) {
         System.out.println("Hola, Mundo!");
+        System.out.println("=============");
     }
     
 }
